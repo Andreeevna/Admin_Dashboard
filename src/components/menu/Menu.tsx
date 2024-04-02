8fcf5b2d8afffd18c122b2e315936661ee064bd7
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2024-04-02 15:39:50 +0300

    implemented the menu component

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,5 +1,27 @@
+import { Link } from 'react-router-dom'
+import { menu } from './../../data.ts'
+import './menu.scss'
+
 const Menu = () => {
-	return <div>Menu</div>
+	return (
+		<div className='menu'>
+			{menu.map(item => {
+				return (
+					<div className='item' key={item.id}>
+						<span className='title'>{item.title}</span>
+						{item?.listItems?.map(listItem => {
+							return (
+								<Link to={listItem.url} className='listItem' key={listItem.id}>
+									<img src={listItem.icon} alt='list-icon' />
+									<span className='listItemTitle'>{listItem.title}</span>
+								</Link>
+							)
+						})}
+					</div>
+				)
+			})}
+		</div>
+	)
 }
 
 export default Menu
