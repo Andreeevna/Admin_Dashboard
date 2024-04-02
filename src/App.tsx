8318f3a15adf64d46dce38cb5c0cf16033334454
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2024-04-02 13:50:26 +0300

    added a router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './pages/home/Home'
+import Products from './pages/products/Products'
+import Users from './pages/users/Users'
 
 function App() {
-	return (
-		<div>
-			<Home />
-		</div>
-	)
+	const router = createBrowserRouter([
+		{
+			path: '/',
+			element: <Home />,
+		},
+		{
+			path: 'products',
+			element: <Products />,
+		},
+		{
+			path: 'users',
+			element: <Users />,
+		},
+	])
+	return <RouterProvider router={router} />
 }
 
 export default App
